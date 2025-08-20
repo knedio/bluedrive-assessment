a6e0c3a786693d161977a5aa45536490bfbf748e
knedio <knedio.info@gmail.com>
2025-08-20 16:03:53 +0800

    added vercel

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  envstr: 'prod',
+  apiBaseUrl: 'https://devtest.bluedrive.io',
+  apiPath: '/api/v1/devtest',
+  apiToken: 'd8efa5fb05336cda75b731ec67e375d28d092ceb',
+};
